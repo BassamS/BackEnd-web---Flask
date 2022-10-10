a82f026c938c1be7828ddd8540ad09e0bd3d2896
Bassam AL SAEDY <basaidi@gmail.com>
2022-10-10 15:12:36 +0200

    Test

--- website/__init__.py
+++ website/__init__.py
@@ -6,4 +6,10 @@ def create_app():
     # SECRET_KEY should be secure safely!
     app.config['SECRET_KEY'] = 'abcdefg h1234'
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
