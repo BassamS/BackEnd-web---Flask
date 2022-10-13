33cb601c43c41a5c470aca8966772889ac70a31a
Bassam AL SAEDY <basaidi@gmail.com>
2022-10-13 15:51:20 +0200

    current user

--- website/__init__.py
+++ website/__init__.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = 'database.db'
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
-    # SECRET_KEY should be secure safely!
-    app.config['SECRET_KEY'] = 'abcdefg h1234'
-    app.config['SQLALCHEMY_DATABASE_URL'] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
@@ -22,6 +22,14 @@ def create_app():
     from .models import User, Note
 
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
